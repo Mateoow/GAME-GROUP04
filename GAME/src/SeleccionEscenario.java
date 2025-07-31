78b30d0f2d3db079a08cc698ef5095056ef30175
favio-tumailli <favio.tumailli@epn.edu.ec>
2025-07-30 19:24:09 -0500

    Creada la clase de seleccion de escenario(dia,noche)

---
+++ GAME/src/SeleccionEscenario.java
@@ -0,0 +1,17 @@
+public class SeleccionEscenario {
+
+    public enum Stage {
+        DIA, NOCHE
+    }
+
+    private Stage stageSeleccionado;
+
+    public Stage getStageSeleccionado() {
+        return stageSeleccionado;
+    }
+
+    public void setStageSeleccionado(Stage stage) {
+        this.stageSeleccionado = stage;
+    }
+}
+
